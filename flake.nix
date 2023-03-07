3c556d238ac2cbfaa2df8df81e04190bf034e68f
Emory Merryman <emory.merryman@gmail.com>
2023-03-06 19:48:24 -0500

    untabify

--- flake.nix
+++ flake.nix
@@ -2,9 +2,9 @@
     inputs =
       {
         flake-utils.url = "github:numtide/flake-utils?rev=5aed5285a952e0b949eb3ba02c12fa4fcfef535f" ;
-	implementation.url = "${IMPLEMENTATION}" ;
-	test.url = "${TEST}" ;
-	tester.url = "github:nextmoose/tester?rev=" e3cdd297e3cb2ba918111ea0ecd627defd2464ca;
+        implementation.url = "${IMPLEMENTATION}" ;
+        test.url = "${TEST}" ;
+        tester.url = "github:nextmoose/tester?rev=" e3cdd297e3cb2ba918111ea0ecd627defd2464ca;
       } ;
     outputs =
       { flake-utils , implementation , test } : flake-utils.lib.eachDefaultSystem ( system : builtins.getAttr system tester.lib implementation test ) ;
--- shell.nix
+++ shell.nix
@@ -3,40 +3,40 @@
     {
       shellHook =
         let
-	  dollar = expression : builtins.concatStringsSep "" [ "$" "{" expression "}" ] ;
-	  in
+          dollar = expression : builtins.concatStringsSep "" [ "$" "{" expression "}" ] ;
+          in
             ''
-	      WORK_DIR=$( ${ pkgs.mktemp }/bin/mktemp --directory ) &&
-	      cd ${ dollar "WORK_DIR" } &&
-	      ${ pkgs.git }/bin/git init &&
-	      ${ pkgs.git }/bin/git config user.name "No One" &&
-	      ${ pkgs.git }/bin/git config user.email "no@one" &&
-	      ${ pkgs.nix }/bin/nix flake --init &&
-	      if [ ${ dollar "IMPLEMENTATION_POSTULATE" } == true ]
-	      then
-	        IMPLEMENTATION=${ dollar "GITHUB_WORKSPACE_REF" }
-	      else
-	        IMPLEMENTATION=${ dollar "IMPLEMENTATION_URL" }
-	      fi &&
-	      if [ ${ dollar "TEST_POSTULATE" } == true ]
-	      then
-	        TEST=${ dollar "GITHUB_WORKSPACE_REF" }
-	      elif [ -z "${ dollar "IMPLEMENTATION_REV" }" ]
-	      then
-	        TEST=${ dollar "TEST_URL" }
-	      else
-	        TEST=${ dollar "TEST_URL" }?rev=${ dollar "TEST_REV" }
-	      fi &&
-	      ${ pkgs.gnused }/bin/sed \
-	        -e "s#\${ dollar "IMPLEMENTATION" }#${ dollar "IMPLEMENTATION" }#" \
-	        -e "s#\${ dollar "TEST" }#${ dollar "TEST" }#" \
-	        -e "wflake.nix" \
-		"${ACTION_PATH}/flake.nix" &&
-	      ${ pkgs.git }/bin/git add flake.nix &&
-	      ${ pkgs.git }/bin/git commit --all --allow-empty-message --message "" &&
-	      if [ ${ dollar "POSTULATE" } == true ]
-	      then
-	        ${ pkgs.nix }/bin/nix develop --command check "${ dollar "TEST_DEFECT" }"
+              WORK_DIR=$( ${ pkgs.mktemp }/bin/mktemp --directory ) &&
+              cd ${ dollar "WORK_DIR" } &&
+              ${ pkgs.git }/bin/git init &&
+              ${ pkgs.git }/bin/git config user.name "No One" &&
+              ${ pkgs.git }/bin/git config user.email "no@one" &&
+              ${ pkgs.nix }/bin/nix flake --init &&
+              if [ ${ dollar "IMPLEMENTATION_POSTULATE" } == true ]
+              then
+                IMPLEMENTATION=${ dollar "GITHUB_WORKSPACE_REF" }
+              else
+                IMPLEMENTATION=${ dollar "IMPLEMENTATION_URL" }
+              fi &&
+              if [ ${ dollar "TEST_POSTULATE" } == true ]
+              then
+                TEST=${ dollar "GITHUB_WORKSPACE_REF" }
+              elif [ -z "${ dollar "IMPLEMENTATION_REV" }" ]
+              then
+                TEST=${ dollar "TEST_URL" }
+              else
+                TEST=${ dollar "TEST_URL" }?rev=${ dollar "TEST_REV" }
+              fi &&
+              ${ pkgs.gnused }/bin/sed \
+                -e "s#\${ dollar "IMPLEMENTATION" }#${ dollar "IMPLEMENTATION" }#" \
+                -e "s#\${ dollar "TEST" }#${ dollar "TEST" }#" \
+                -e "wflake.nix" \
+                "${ACTION_PATH}/flake.nix" &&
+              ${ pkgs.git }/bin/git add flake.nix &&
+              ${ pkgs.git }/bin/git commit --all --allow-empty-message --message "" &&
+              if [ ${ dollar "POSTULATE" } == true ]
+              then
+                ${ pkgs.nix }/bin/nix develop --command check "${ dollar "TEST_DEFECT" }"
               fi
             '' ;
     }
