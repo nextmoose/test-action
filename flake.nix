5e34d2a84e6271999f2feecd830a11342ff2c174
Emory Merryman <emory.merryman@gmail.com>
2023-03-13 14:39:56 -0400

    use new test

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
         flake-utils.url = "github:numtide/flake-utils?rev=5aed5285a952e0b949eb3ba02c12fa4fcfef535f" ;
         implementation.url = "${IMPLEMENTATION}" ;
         test.url = "${TEST}" ;
-        tester.url = "github:nextmoose/tester?rev=e3cdd297e3cb2ba918111ea0ecd627defd2464ca" ;
+        tester.url = "github:nextmoose/tester?rev=aac01d11f1f1c31092ce5f6bd0cacf589d6804c8" ;
       } ;
     outputs =
       { flake-utils , implementation , self , test , tester } : flake-utils.lib.eachDefaultSystem ( system : builtins.getAttr system tester.lib implementation test ) ;
